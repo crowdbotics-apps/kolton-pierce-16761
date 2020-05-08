034ccfa5bbb8ede6d10f5032037e7e066704ff9d
Crowdbotics <team@crowdbotics.com>
2020-05-08 22:54:28 +0000

    Update models

--- backend/kolton_pierce_stree_16761/wsgi.py
+++ backend/kolton_pierce_stree_16761/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kolton_pierce_stree_16761.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kolton_pierce_stree_16761.settings")
 
 application = get_wsgi_application()
